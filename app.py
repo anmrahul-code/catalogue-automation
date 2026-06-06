309b076bcafa3d0932380362fed3b8de7a144ebb
anmrahul-code <anmrahul@gmail.com>
2026-06-07 00:09:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -137,41 +137,14 @@ if st.button("Generate Template"):
 
                 except:
                     pass
+
+        # =====================
         # Filter Category
-        if marketplace == "Flipkart" and "Gender" in master_df.columns:
-        gender_series = (master_df["Gender"]
-        .astype(str)
-        .str.strip()
-        .str.upper()
-    )
-
-    if selected_category == "Kids Tshirts":
-
-        category_df = master_df[
-            (master_df["Final Category"] == "TSHIRTS") &
-            (gender_series.isin(["BOYS", "GIRLS"]))
-        ].copy()
-
-    elif selected_category == "Tshirts":
-
-        category_df = master_df[
-            (master_df["Final Category"] == "TSHIRTS") &
-            (gender_series.isin(["MEN", "WOMEN"]))
-        ].copy()
-
-    else:
-
+        # =====================
         category_df = master_df[
             master_df["Final Category"]
             == selected_category.upper()
         ].copy()
-
-else:
-
-    category_df = master_df[
-        master_df["Final Category"]
-        == selected_category.upper()
-    ].copy()
 
         # =====================
         # Detect Template Column
