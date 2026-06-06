d54404002da2412da9e15160f3d6a444efdeaec8
anmrahul-code <anmrahul@gmail.com>
2026-06-06 23:54:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -140,8 +140,7 @@ if st.button("Generate Template"):
 
 # Filter Category
 # =====================
-(
-if marketplace == "Flipkart" and "Gender" in master_df.columns:
+(if marketplace == "Flipkart" and "Gender" in master_df.columns:
     gender_series = (master_df["Gender"]
         .astype(str)
         .str.strip()
