d3c4c963f3e028bf295b089acd30f1fe06729a4f
anmrahul-code <anmrahul@gmail.com>
2026-06-07 11:00:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -139,51 +139,39 @@ if st.button("Generate Template"):
                     pass
 
                # =====================
-        # Filter Category
         # =====================
+# Filter Category
+# =====================
 
-        category_df = master_df[
-            master_df["Final Category"]
-            == selected_category.upper()
-        ].copy()
+if marketplace == "Myntra":
 
-        # Flipkart Kids Tshirts Logic
-        if (
-            marketplace == "Flipkart"
-            and selected_category == "Kids Tshirts"
-            and "Gender" in master_df.columns
-        ):
+    category_col = "Myntra Category"
 
-            category_df = master_df[
-                (master_df["Final Category"] == "TSHIRTS")
-                &
-                (
-                    master_df["Gender"]
-                    .astype(str)
-                    .str.strip()
-                    .str.upper()
-                    .isin(["BOYS", "GIRLS"])
-                )
-            ].copy()
+elif marketplace == "Flipkart":
 
-        # Flipkart Adult Tshirts Logic
-        elif (
-            marketplace == "Flipkart"
-            and selected_category == "Tshirts"
-            and "Gender" in master_df.columns
-        ):
+    category_col = "Flipkart Category"
 
-            category_df = master_df[
-                (master_df["Final Category"] == "TSHIRTS")
-                &
-                (
-                    master_df["Gender"]
-                    .astype(str)
-                    .str.strip()
-                    .str.upper()
-                    .isin(["MEN", "WOMEN"])
-                )
-            ].copy()
+elif marketplace == "Ajio":
+
+    category_col = "Ajio Category"
+
+else:
+
+    category_col = "Final Category"
+
+if category_col not in master_df.columns:
+
+    st.error(f"{category_col} column not found in Master File")
+    st.stop()
+
+category_df = master_df[
+    master_df[category_col]
+    .astype(str)
+    .str.strip()
+    .str.upper()
+    == selected_category.upper()
+].copy()
+       
 
         # =====================
         # Detect Template Column
